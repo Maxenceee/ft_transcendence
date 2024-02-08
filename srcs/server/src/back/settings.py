9bbc54c356de0d591f3cb5ec734a460892ee5002
Stelirion <noegennaro73@gmail.com>
2024-02-08 22:03:55 +0100

    add static folder

--- srcs/server/src/back/settings.py
+++ srcs/server/src/back/settings.py
@@ -116,7 +116,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+#I want ton setup my static files in the client folder, my static file are stored in the ../client/public folder
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "client/public"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
