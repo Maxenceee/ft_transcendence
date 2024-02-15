e5e114869b74ae2c0c19aa5e68abb9645f7abffb
Maxenceee <72473014+Maxenceee@users.noreply.github.com>
2024-02-15 19:08:14 +0100

    new home page

--- srcs/express.tmp/app.js
+++ srcs/express.tmp/app.js
@@ -92,7 +92,10 @@ app.use(cors({
 app.get('/:page', (req, res, next) => {
 	if (!req.params.page || req.params.page == "index.html")
 		return res.status(200).sendFile(path.join(process.env["CLIENT_BUILD_BIR"], 'views/index.html'));
-	res.status(200).sendFile(path.join(process.env["CLIENT_BUILD_BIR"], 'views/'+req.params.page));
+	let p = path.join(process.env["CLIENT_BUILD_BIR"], 'views/'+req.params.page);
+	if (!fs.existsSync(p))
+		return next();
+	res.status(200).sendFile(p);
 });
 
 app.use((_, res) => {
