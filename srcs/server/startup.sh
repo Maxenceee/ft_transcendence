cf985d58645def340071dd77a60254d985d513b7
Maxenceee <72473014+Maxenceee@users.noreply.github.com>
2024-02-21 15:41:26 +0100

    docker migrate bug

--- srcs/server/startup.sh
+++ srcs/server/startup.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
+echo "Starting Django Server"
+
+python3 manage.py makemigrations
+
+echo "migrations prapared"
+
 python3 manage.py migrate
+
+echo "migrations done"
+
+python manage.py showmigrations
+
+echo "migrations shown"
 
 python3 manage.py runserver 0.0.0.0:3000
