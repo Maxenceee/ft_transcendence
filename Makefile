3d35ee187c8d0859d65035b2a8f8cdc71922c139
Stelirion <noegennaro73@gmail.com>
2024-02-21 14:41:31 +0100

    ca remarche. pourquoi ? trop de questions...

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 SERVER_DIR	=	srcs
-NAME		=	docker_django
+NAME		=	django
 FORWARDPORT	=	3000
 
 GREEN			=	\033[1;32m
@@ -11,7 +11,7 @@ DEFAULT			=	\033[0m
 all: up
 
 debug: all
-	@docker logs docker_django -f
+	@docker logs $(NAME) -f
 
 up:
 	@printf "$(GREEN)Building and running the container...$(DEFAULT)\n"
@@ -35,6 +35,11 @@ clean: down
 	@printf "$(BLUE)Docker has been cleaned\n"
 
 fclean: clean
+	@printf "$(RED)Cleaning data...$(DEFAULT)\n"
+	@./clean.sh
+	@printf "$(BLUE)Data has been cleaned\n"
+
+rmdata: down
 	@printf "$(RED)Cleaning data...$(DEFAULT)\n"
 	@./clean.sh
 	@printf "$(BLUE)Data has been cleaned\n"
--- srcs/server/index/views.py
+++ srcs/server/index/views.py
@@ -2,7 +2,11 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import *
 from .decorators import *
+from passlib.hash import django_pbkdf2_sha256 as pbkdf2
 import logging
+
+def makeid(length):
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
 
 # Create your views here.
 @login_required
@@ -27,14 +31,14 @@ def login(request):
         if not username or not password:
             return render(request, 'views/connection.html', {"login": username, "is_invalid": True})
         
-        if User.objects.filter(username=username, password=password).exists():
-            user = User.objects.get(username=username, password=password)
-            token=''.join(random.choices(string.ascii_letters + string.digits, k=100))
-
-            response = redirect("/")
-            response.set_cookie(key='token', value=token, httponly=True, expires=7*24*60*60, samesite='Lax')
-            Token.objects.create(token=token, user=user)
-            return response
+        if User.objects.filter(username=username).exists():
+            user = User.objects.get(username=username)
+            if pbkdf2.verify(password, user.password):
+                token = makeid(100)
+                response = redirect("/")
+                Token.objects.create(token=token, user=user)
+                response.set_cookie(key='token', value=token, httponly=True, expires=7*24*60*60, samesite='Lax')
+                return response
 
         return render(request, 'views/connection.html', {"login": username, "is_invalid": True})
 
@@ -51,12 +55,13 @@ def signup(request):
         if User.objects.filter(username=username).exists():
             return render(request, 'views/connection.html', {"login": username, "is_signup": True, "action_url": "/signup", "exists": True})
         
+        password = pbkdf2.hash(password)
         user = User.objects.create(username=username, password=password)
         response = redirect("/")
 
-        token=''.join(random.choices(string.ascii_letters + string.digits, k=100))
-        response.set_cookie(key='token', value=token, httponly=True, expires=7*24*60*60, samesite='Lax')
+        token = makeid(100)
         Token.objects.create(token=token, user=user)
+        response.set_cookie(key='token', value=token, httponly=True, expires=7*24*60*60, samesite='Lax')
         return response
 
 @login_required
