a8457b07edbe2505db627f50eaa4785f2c65f346
mbrement <micka.breme@gmail.com>
2024-03-14 11:20:42 +0100

    god help me

--- srcs/server/game_classic/consumers.py
+++ srcs/server/game_classic/consumers.py
@@ -133,8 +133,8 @@ class Game:
 			self.ball.speed = 5
 
 	def rebound_z(self, playerID):
-		if ((self.ball.x < -27 and playerID == 2) or (self.ball.x > 27 and playerID == 3)) and (self.ball.z < (self.players[playerID].pad_z + 4.5)  and self.ball.x > (self.players[playerID].pad_z - 4.5)):
-			if (playerID == 1) :
+		if ((self.ball.x < -27 and playerID == 2) or (self.ball.x > 27 and playerID == 3)) and (self.ball.z < (self.players[playerID].pad_z + 4.5)  and self.ball.z > (self.players[playerID].pad_z - 4.5)):
+			if (playerID == 2) :
 				self.ball.direction_z = (self.ball.x - self.players[playerID].pad_x)/4.5
 				self.ball.direction_x = 1
 			else :
