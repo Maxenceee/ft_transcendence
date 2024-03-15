75c598152f52c5a0bcc1bbd964dafa3c0c567c23
Maxenceee <72473014+Maxenceee@users.noreply.github.com>
2024-03-15 15:01:18 +0100

    Update renderer.js

--- srcs/client/public/javascripts/renderer.js
+++ srcs/client/public/javascripts/renderer.js
@@ -521,7 +521,7 @@ class UserPagePlayerStats extends Component {
 
 	isWinner(data) {
 		data = data.map(e => {
-			let p = e.data.sort((a, b) => a.score < b.score);
+			let p = e.data.sort((a, b) => a.score < b.score ? 1 : -1);
 			return p[0].id == this.state.user.id;
 		});
 		return data;
@@ -627,7 +627,7 @@ class UserPagePlayerHistory extends Component {
 			createElement('div', {
 				class: "history-card-content", children: (
 					(this.state.user.game_history && this.state.user.game_history.length) ?
-					this.state.user.game_history.sort((a, b) => a.date < b.date).map(game => {
+					this.state.user.game_history.sort((a, b) => a.date > b.date ? -1 : 1).map(game => {
 						let p = game.data.sort((a, b) => a.score > b.score ? -1 : 1)[0].id == this.state.user.id;
 						let s = game.data.sort((_, b) => b.id == this.state.user.id);
 						if (game.type === "2p") {
