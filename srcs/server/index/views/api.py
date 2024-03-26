9af2cbc8625ecd623e2d02347325febad4f1f92a
Stelirion <noegennaro73@gmail.com>
2024-03-26 15:47:50 +0100

    securise bad request

--- srcs/server/index/views/api.py
+++ srcs/server/index/views/api.py
@@ -9,6 +9,8 @@ from django.core.files.storage import default_storage
 
 @login_required
 def api_get_user(request, id):
+	if request.method != 'GET':
+		return JsonResponse({'error': 'Method not allowed'}, status=405)
 	if id == "me":
 		id = request.COOKIES.get('token')
 		user = Token.objects.get(token=id).user
@@ -20,36 +22,45 @@ def api_get_user(request, id):
 
 @login_required
 def api_update_user(request):
+	if request.method != 'POST':
+		return JsonResponse({'error': 'Method not allowed'}, status=405)
 	id = request.COOKIES.get('token')
 	user = Token.objects.get(token=id).user
 	if 'nickname' not in request.POST:
 		return JsonResponse({'error': 'Missing nickname'}, status=400)
+	if len(request.POST['nickname']) < 3:
+		return JsonResponse({'error': 'Nickname too short'}, status=400)
+	if len(request.POST['nickname']) > 20:
+		return JsonResponse({'error': 'Nickname too long'}, status=400)
 	user.nickname = request.POST['nickname']
 	user.save()
 	return JsonResponse({'message': 'Succes'}, status=200)
 
 @login_required
 def api_update_picture(request):
-	if request.method == 'POST':
-		id = request.COOKIES.get('token')
-		user = Token.objects.get(token=id).user
+	if request.method != 'POST':
+		return JsonResponse({'error': 'Method not allowed'}, status=405)
+	id = request.COOKIES.get('token')
+	user = Token.objects.get(token=id).user
 
-		if 'file' in request.FILES:
-			profile_picture = request.FILES['file']
-			content_type = profile_picture.content_type
+	if 'file' in request.FILES:
+		profile_picture = request.FILES['file']
+		content_type = profile_picture.content_type
 
-			if content_type not in ['image/jpeg', 'image/png']:
-				return JsonResponse({'error': 'Format de fichier non supporté. Seuls les fichiers JPEG et PNG sont acceptés.'}, status=400)
+		if content_type not in ['image/jpeg', 'image/png']:
+			return JsonResponse({'error': 'Format de fichier non supporté. Seuls les fichiers JPEG et PNG sont acceptés.'}, status=400)
 
-			user.profile_picture_image = profile_picture
-			user.save()
-			return JsonResponse({'message': 'Photo de profil mise à jour avec succès.'})
-		else:
-			return JsonResponse({'error': 'Aucun fichier téléchargé.'}, status=400)
+		user.profile_picture_image = profile_picture
+		user.save()
+		return JsonResponse({'message': 'Photo de profil mise à jour avec succès.'})
+	else:
+		return JsonResponse({'error': 'Aucun fichier téléchargé.'}, status=400)
 		
 
 @login_required
 def api_avatar(request, id):
+	if request.method != 'GET':
+		return JsonResponse({'error': 'Method not allowed'}, status=405)
 	try:
 		logging.info(id)
 		with default_storage.open(f"avatar/{id}", 'rb') as img_file:
@@ -62,6 +73,8 @@ def api_avatar(request, id):
 	
 @login_required
 def api_search_user(request, id):
+	if request.method != 'GET':
+		return JsonResponse({'error': 'Method not allowed'}, status=405)
 	users = User.objects.filter(nickname__contains=id)
 	response = []
 	for user in users:
@@ -70,6 +83,8 @@ def api_search_user(request, id):
 
 @login_required
 def api_follow(request, id):
+	if request.method != 'POST':
+		return JsonResponse({'error': 'Method not allowed'}, status=405)
 	user = Token.objects.get(token=request.COOKIES.get('token')).user
 	user_to_follow = get_object_or_404(User, id=id)
 	user.following.add(user_to_follow)
@@ -77,6 +92,8 @@ def api_follow(request, id):
 
 @login_required
 def api_unfollow(request, id):
+	if request.method != 'POST':
+		return JsonResponse({'error': 'Method not allowed'}, status=405)
 	user = Token.objects.get(token=request.COOKIES.get('token')).user
 	user_to_unfollow = get_object_or_404(User, id=id)
 	user.following.remove(user_to_unfollow)
