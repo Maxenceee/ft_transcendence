3657dd1eb469ba6df16eddd359156643392cd3bf
Stelirion <noegennaro73@gmail.com>
2024-03-28 18:07:50 +0100

    improve

--- srcs/client/srcs/components/main/FriendsPanel.js
+++ srcs/client/srcs/components/main/FriendsPanel.js
@@ -68,8 +68,8 @@ class FriendsPanel extends Component {
 
 	playerRow(player) {
 		let action = [
-			{title: "Follow", color: "var(--friend-bg)", hover: "var(--friend-bg-h)", action: this.followPlayer},
-			{title: "Unfollow", color: "#8e8e8e", hover: "#555555", action: this.unfollowPlayer}
+			{title: "Suivre", color: "var(--friend-bg)", hover: "var(--friend-bg-h)", action: this.followPlayer},
+			{title: "Ne plus suivre", color: "#8e8e8e", hover: "#555555", action: this.unfollowPlayer}
 		]
 		let isfr = (e) => this.state.following.find(f => f.id == e);
 		let opt = action[isfr(player.id) ? 1 : 0];
@@ -122,7 +122,7 @@ class FriendsPanel extends Component {
 					createElement('div', {
 						class: "friend-container", children: [
 							createElement('div', {
-								class: "friend-search", style: (this.state.open ? "width: 280px" : ""), children: [
+								class: "friend-search", style: (this.state.open ? "width: 450px" : ""), children: [
 									this.state.open && createElement('div', {
 										class: "friend-search-header", children: [
 											createElement('div', {
@@ -135,12 +135,12 @@ class FriendsPanel extends Component {
 											this.state.onsearch ?
 											createElement('div', {
 												class: "friend-search-input", children: createElement('input', {
-													ref: this.ref, value: this.state.search, onkeypress: this.searchPlayer, placeholder: "Search", oninput: e => this.setState({search: e.target.value})
+													ref: this.ref, value: this.state.search, onkeypress: this.searchPlayer, placeholder: "Rechercher", oninput: e => this.setState({search: e.target.value})
 												})
 											})
 											:
 											createElement('div', {
-												class: "friend-search-label", children: "Friends"
+												class: "friend-search-label", children: "Amis"
 											})
 										]
 									}),
