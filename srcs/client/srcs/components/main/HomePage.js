2b0ad6cd23638916abe7c7f2e90d145cb5886799
Maxenceee <72473014+Maxenceee@users.noreply.github.com>
2024-03-28 20:23:25 +0100

    home page

--- srcs/client/srcs/components/main/HomePage.js
+++ srcs/client/srcs/components/main/HomePage.js
@@ -43,13 +43,32 @@ class HomePage extends Component {
 																class: "content", style: "background-image: url('/static/images/pong_visual_1.png');", children: createElement('div', {
 																	class: "card-container", children: [
 																		link({
-																			to: "/game/2p", children: createElement('div', {
+																			to: "/game/2p", children: 
+																			createElement('div', {
 																				class: "play-button", children: createElement('svg', {
 																					width: "28", height: "28", viewBox: "0 0 28 28", fill: "none", xmlns: "http://www.w3.org/2000/svg", children: createElement('path', {
 																						d: "M5.32789 25.4892H23.4434C25.4884 25.4892 26.7692 24.0199 26.7692 22.1784C26.7692 21.6108 26.6073 21.0197 26.3037 20.4851L17.2332 4.67086C16.5964 3.55805 15.5091 3 14.3867 3C13.2622 3 12.159 3.56227 11.536 4.67086L2.46547 20.4872C2.14484 21.0293 2 21.6108 2 22.1784C2 24.0199 3.28086 25.4892 5.32789 25.4892Z"
 																					})
 																				})
 																			})
+																			// createElement('ul', {
+																			// 	class: "bc-btn-transition reserv-btn", children: [
+																			// 		createElement('li', {
+																			// 			class: "bc-btn-transition", children: createElement('span', {
+																			// 				children: link({
+																			// 					class: "bc-btn-transition", children: "Réserver"
+																			// 				})
+																			// 			})
+																			// 		}),
+																			// 		createElement('li', {
+																			// 			class: "bc-btn-transition", children: createElement('span', {
+																			// 				children: createElement('i', {
+																			// 					class: "fa fa-chevron-down bc-btn-transition", "aria-hidden": "true"
+																			// 				})
+																			// 			})
+																			// 		})
+																			// 	]
+																			// })
 																		}),
 																		createElement('div', {
 																			class: "lobby-name", children: createElement('h2', {
