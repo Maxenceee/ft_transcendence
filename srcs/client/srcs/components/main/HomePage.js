195eb31a4f07879b6e89ea74ed50cb8a7e32da26
Maxenceee <72473014+Maxenceee@users.noreply.github.com>
2024-03-29 20:32:24 +0100

    play button

--- srcs/client/srcs/components/main/HomePage.js
+++ srcs/client/srcs/components/main/HomePage.js
@@ -17,6 +17,29 @@ class HomePage extends Component {
 		// console.log("this.componentWillUnmount HomePage Page");
 	}
 
+	playButton() {
+		return createElement('div', {
+			class: "bc-price-btn", children: createElement('ul', {
+				class: "bc-btn-transition reserv-btn", children: [
+					createElement('li', {
+						class: "bc-btn-transition", children: createElement('span', {
+							children: "Jouer"
+						})
+					}),
+					createElement('li', {
+						class: "bc-btn-transition", children: createElement('div', {
+							class: "bc-btn-transition", children: createElement('svg', {
+								class: "icon bc-btn-transition", width: "40", height: "40", viewBox: "0 0 28 28", fill: "none", xmlns: "http://www.w3.org/2000/svg", children: createElement('path', {
+									d: "M5.32789 25.4892H23.4434C25.4884 25.4892 26.7692 24.0199 26.7692 22.1784C26.7692 21.6108 26.6073 21.0197 26.3037 20.4851L17.2332 4.67086C16.5964 3.55805 15.5091 3 14.3867 3C13.2622 3 12.159 3.56227 11.536 4.67086L2.46547 20.4872C2.14484 21.0293 2 21.6108 2 22.1784C2 24.0199 3.28086 25.4892 5.32789 25.4892Z"
+								})
+							})
+						})
+					})
+				]
+			})
+		})
+	}
+
 	render() {
 		return createElement('div', {
 			children: createElement('div', {
@@ -48,34 +71,7 @@ class HomePage extends Component {
 																class: "content", style: "background-image: url('/static/images/pong_visual_1.png');", children: createElement('div', {
 																	class: "card-container", children: [
 																		link({
-																			to: "/game/2p", children: 
-																			// createElement('div', {
-																			// 	class: "play-button", children: createElement('svg', {
-																			// 		width: "28", height: "28", viewBox: "0 0 28 28", fill: "none", xmlns: "http://www.w3.org/2000/svg", children: createElement('path', {
-																			// 			d: "M5.32789 25.4892H23.4434C25.4884 25.4892 26.7692 24.0199 26.7692 22.1784C26.7692 21.6108 26.6073 21.0197 26.3037 20.4851L17.2332 4.67086C16.5964 3.55805 15.5091 3 14.3867 3C13.2622 3 12.159 3.56227 11.536 4.67086L2.46547 20.4872C2.14484 21.0293 2 21.6108 2 22.1784C2 24.0199 3.28086 25.4892 5.32789 25.4892Z"
-																			// 		})
-																			// 	})
-																			// })
-																			createElement('div', {
-																				class: "bc-price-btn", children: createElement('ul', {
-																					class: "bc-btn-transition reserv-btn", children: [
-																						createElement('li', {
-																							class: "bc-btn-transition", children: createElement('span', {
-																								children: "Jouer"
-																							})
-																						}),
-																						createElement('li', {
-																							class: "bc-btn-transition", children: createElement('div', {
-																								class: "bc-btn-transition", children: createElement('svg', {
-																									class: "icon bc-btn-transition", width: "40", height: "40", viewBox: "0 0 28 28", fill: "none", xmlns: "http://www.w3.org/2000/svg", children: createElement('path', {
-																										d: "M5.32789 25.4892H23.4434C25.4884 25.4892 26.7692 24.0199 26.7692 22.1784C26.7692 21.6108 26.6073 21.0197 26.3037 20.4851L17.2332 4.67086C16.5964 3.55805 15.5091 3 14.3867 3C13.2622 3 12.159 3.56227 11.536 4.67086L2.46547 20.4872C2.14484 21.0293 2 21.6108 2 22.1784C2 24.0199 3.28086 25.4892 5.32789 25.4892Z"
-																									})
-																								})
-																							})
-																						})
-																					]
-																				})
-																			})
+																			to: "/game/2p", children: this.playButton()
 																		}),
 																		createElement('div', {
 																			class: "lobby-name", children: createElement('h2', {
@@ -93,13 +89,7 @@ class HomePage extends Component {
 																class: "content", style: "background-image: url('/static/images/pong_visual_3.png');", children: createElement('div', {
 																	class: "card-container", children: [
 																		link({
-																			to: "/game/4p", children: createElement('div', {
-																				class: "play-button", children: createElement('svg', {
-																					width: "28", height: "28", viewBox: "0 0 28 28", fill: "none", xmlns: "http://www.w3.org/2000/svg", children: createElement('path', {
-																						d: "M5.32789 25.4892H23.4434C25.4884 25.4892 26.7692 24.0199 26.7692 22.1784C26.7692 21.6108 26.6073 21.0197 26.3037 20.4851L17.2332 4.67086C16.5964 3.55805 15.5091 3 14.3867 3C13.2622 3 12.159 3.56227 11.536 4.67086L2.46547 20.4872C2.14484 21.0293 2 21.6108 2 22.1784C2 24.0199 3.28086 25.4892 5.32789 25.4892Z"
-																					})
-																				})
-																			})
+																			to: "/game/4p", children: this.playButton()
 																		}),
 																		createElement('div', {
 																			class: "lobby-name", children: createElement('h2', {
@@ -117,13 +107,7 @@ class HomePage extends Component {
 																class: "content", style: "background-image: url('/static/images/pong_visual_11.png');", children: createElement('div', {
 																	class: "card-container", children: [
 																		link({
-																			to: "/game/tournament", children: createElement('div', {
-																				class: "play-button", children: createElement('svg', {
-																					width: "28", height: "28", viewBox: "0 0 28 28", fill: "none", xmlns: "http://www.w3.org/2000/svg", children: createElement('path', {
-																						d: "M5.32789 25.4892H23.4434C25.4884 25.4892 26.7692 24.0199 26.7692 22.1784C26.7692 21.6108 26.6073 21.0197 26.3037 20.4851L17.2332 4.67086C16.5964 3.55805 15.5091 3 14.3867 3C13.2622 3 12.159 3.56227 11.536 4.67086L2.46547 20.4872C2.14484 21.0293 2 21.6108 2 22.1784C2 24.0199 3.28086 25.4892 5.32789 25.4892Z"
-																					})
-																				})
-																			})
+																			to: "/game/tournament", children: this.playButton()
 																		}),
 																		createElement('div', {
 																			class: "lobby-name", children: createElement('h2', {
@@ -152,13 +136,7 @@ class HomePage extends Component {
 																class: "content", style: "background-image: url('/static/images/pong_visual_1.png');", children: createElement('div', {
 																	class: "card-container", children: [
 																		link({
-																			to: "/game/ai", children: createElement('div', {
-																				class: "play-button", children: createElement('svg', {
-																					width: "28", height: "28", viewBox: "0 0 28 28", fill: "none", xmlns: "http://www.w3.org/2000/svg", children: createElement('path', {
-																						d: "M5.32789 25.4892H23.4434C25.4884 25.4892 26.7692 24.0199 26.7692 22.1784C26.7692 21.6108 26.6073 21.0197 26.3037 20.4851L17.2332 4.67086C16.5964 3.55805 15.5091 3 14.3867 3C13.2622 3 12.159 3.56227 11.536 4.67086L2.46547 20.4872C2.14484 21.0293 2 21.6108 2 22.1784C2 24.0199 3.28086 25.4892 5.32789 25.4892Z"
-																					})
-																				})
-																			})
+																			to: "/game/ai", children: this.playButton()
 																		}),
 																		createElement('div', {
 																			class: "lobby-name", children: createElement('h2', {
@@ -176,13 +154,7 @@ class HomePage extends Component {
 																class: "content", style: "background-image: url('/static/images/pong_visual_8.png');", children: createElement('div', {
 																	class: "card-container", children: [
 																		link({
-																			to: "/game/local", children: createElement('div', {
-																				class: "play-button", children: createElement('svg', {
-																					width: "28", height: "28", viewBox: "0 0 28 28", fill: "none", xmlns: "http://www.w3.org/2000/svg", children: createElement('path', {
-																						d: "M5.32789 25.4892H23.4434C25.4884 25.4892 26.7692 24.0199 26.7692 22.1784C26.7692 21.6108 26.6073 21.0197 26.3037 20.4851L17.2332 4.67086C16.5964 3.55805 15.5091 3 14.3867 3C13.2622 3 12.159 3.56227 11.536 4.67086L2.46547 20.4872C2.14484 21.0293 2 21.6108 2 22.1784C2 24.0199 3.28086 25.4892 5.32789 25.4892Z"
-																					})
-																				})
-																			})
+																			to: "/game/local", children: this.playButton()
 																		}),
 																		createElement('div', {
 																			class: "lobby-name", children: createElement('h2', {
