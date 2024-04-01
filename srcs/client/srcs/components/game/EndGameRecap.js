7f704b6d0bb10a346cb95d7482dbb90dbc4e4842
Maxenceee <72473014+Maxenceee@users.noreply.github.com>
2024-04-01 21:40:30 +0200

    test view

--- srcs/client/srcs/components/game/EndGameRecap.js
+++ srcs/client/srcs/components/game/EndGameRecap.js
@@ -5,14 +5,44 @@ class EndGameRecap extends Component {
 		super(props);
 	}
 
-	render() {
-		console.log("EndGameRecap", this.props);
-		return createElement('div', {
-			class: "dg-overlay", children: this.props && this.props.data.sort((a, b) => a.score < b.score ? -1 : 1).map((player, i) => {
-				return player.id;
+	twoPlayers() {
+		return [
+			createElement('div', {
+				class: "dg-overlay-player", children: [
+					createElement('h1', {
+						children: this.state.players[0].nickname,
+					}),
+					createElement('img', {
+						src: this.state.players[0].profile_picture
+					}),
+				]
 			}),
+			createElement('div', {
+				
+			}),
+			createElement('div', {
+				class: "dg-overlay-player", 
+			}),
+		]
+	}
+
+	fourPlayers() {
+
+	}
+
+	render() {
+		return createElement('div', {
+			class: "dg-overlay", children: createElement('div', {
+				class: "dg-overlay-cnt", children: this.props.data.length == 2 ?
+				this.twoPlayers()
+				:
+				this.fourPlayers()
+			})
 		});
 	}
+	// children: this.props && this.props.data.sort((a, b) => a.score < b.score ? -1 : 1).map((player, i) => {
+	// 	return player.id;
+	// }),
 }
 
 export default EndGameRecap;
