34cf7e8f0800913c0238a9be3e879d6ad7356e5f
Stelirion <noegennaro73@gmail.com>
2024-04-10 10:46:37 +0200

    paranoia

--- srcs/server/index/views/api.py
+++ srcs/server/index/views/api.py
@@ -9,66 +9,73 @@ from django.core.files.storage import default_storage
 
 @login_required
 def api_get_user(request, id):
-	if request.method != 'GET':
-		return JsonResponse({'error': 'Method not allowed'}, status=405)
-	if id == "me":
-		id = request.COOKIES.get('token')
-		user = Token.objects.get(token=id).user
-	else:
-		user = get_object_or_404(User, id=id)
-	response = user.to_json()
-
-	return JsonResponse(response)
+	try:
+		if request.method != 'GET':
+			return JsonResponse({'error': 'Method not allowed'}, status=405)
+		if id == "me":
+			id = request.COOKIES.get('token')
+			user = Token.objects.get(token=id).user
+		else:
+			user = get_object_or_404(User, id=id)
+		response = user.to_json()
+		
+		return JsonResponse(response)
+	except:
+		return JsonResponse({'error': 'Bad request'}, status=400)
 
 @login_required
 def api_get_user_following(request, id):
-	if request.method != 'GET':
-		return JsonResponse({'error': 'Method not allowed'}, status=405)
-	if id == "me":
-		id = request.COOKIES.get('token')
-		user = Token.objects.get(token=id).user
-	else:
-		user = get_object_or_404(User, id=id)
-	following = []
-	for user in user.following.all():
-		following.append(user.resume_to_json())
-	response = {
-		"following": following,
-	}
+	try:
+		if request.method != 'GET':
+			return JsonResponse({'error': 'Method not allowed'}, status=405)
+		if id == "me":
+			id = request.COOKIES.get('token')
+			user = Token.objects.get(token=id).user
+		else:
+			user = get_object_or_404(User, id=id)
+		following = []
+		for user in user.following.all():
+			following.append(user.resume_to_json())
+		response = {
+			"following": following,
+		}
 
-	return JsonResponse(response)
+		return JsonResponse(response)
+	except:
+		return JsonResponse({'error': 'Bad request'}, status=400)
 
 @login_required
 def api_update_user(request):
-	if request.method != 'POST':
-		return JsonResponse({'error': 'Method not allowed'}, status=405)
-	id = request.COOKIES.get('token')
-	user = Token.objects.get(token=id).user
+	try:
+		if request.method != 'POST':
+			return JsonResponse({'error': 'Method not allowed'}, status=405)
+		id = request.COOKIES.get('token')
+		user = Token.objects.get(token=id).user
 
-	try :
-		data = json.loads(request.body)
+		try :
+			data = json.loads(request.body)
+		except:
+			return JsonResponse({'error': 'Invalid JSON'}, status=400)
+
+		if 'nickname' not in data:
+			return JsonResponse({'error': 'Missing nickname'}, status=400)
+		if len(data['nickname']) < 3:
+			return JsonResponse({'error': 'Nickname too short'}, status=400)
+		if len(data['nickname']) > 20:
+			return JsonResponse({'error': 'Nickname too long'}, status=400)
+		user.nickname = data['nickname']
+		user.save()
+		return JsonResponse({'message': 'Succes'}, status=200)
 	except:
-		return JsonResponse({'error': 'Invalid JSON'}, status=400)
-
-	if 'nickname' not in data:
-		return JsonResponse({'error': 'Missing nickname'}, status=400)
-	if len(data['nickname']) < 3:
-		return JsonResponse({'error': 'Nickname too short'}, status=400)
-	if len(data['nickname']) > 20:
-		return JsonResponse({'error': 'Nickname too long'}, status=400)
-	user.nickname = data['nickname']
-	user.save()
-	return JsonResponse({'message': 'Succes'}, status=200)
+		return JsonResponse({'error': 'Bad request'}, status=400)
 
 @login_required
 def api_update_picture(request):
-	if request.method != 'POST':
-		return JsonResponse({'error': 'Method not allowed'}, status=405)
-	id = request.COOKIES.get('token')
-	user = Token.objects.get(token=id).user
-
-	logging.info(request.FILES)
-	if 'profile_picture' in request.FILES:
+	try:
+		if request.method != 'POST':
+			return JsonResponse({'error': 'Method not allowed'}, status=405)
+		id = request.COOKIES.get('token')
+		user = Token.objects.get(token=id).user
 		profile_picture = request.FILES['profile_picture']
 		content_type = profile_picture.content_type
 
@@ -78,76 +85,90 @@ def api_update_picture(request):
 		user.profile_picture_image = profile_picture
 		user.save()
 		return JsonResponse({'message': 'Photo de profil mise à jour avec succès.'})
-	else:
-		return JsonResponse({'error': 'Aucun fichier téléchargé.'}, status=400)
+	except:
+		return JsonResponse({'error': 'Bad request'}, status=400)
 
 @login_required
 def api_delete_picture(request):
-	if request.method != 'POST':
-		return JsonResponse({'error': 'Method not allowed'}, status=405)
-	id = request.COOKIES.get('token')
-	user = Token.objects.get(token=id).user
-	user.profile_picture_image = None
-	user.save()
-	return JsonResponse({'message': 'Photo de profil supprimée avec succès.'})
+	try:
+		if request.method != 'POST':
+			return JsonResponse({'error': 'Method not allowed'}, status=405)
+		id = request.COOKIES.get('token')
+		user = Token.objects.get(token=id).user
+		user.profile_picture_image = None
+		user.save()
+		return JsonResponse({'message': 'Photo de profil supprimée avec succès.'})
+	except:
+		return JsonResponse({'error': 'Bad request'}, status=400)
 		
 
 @login_required
 def api_avatar(request, id):
-	if request.method != 'GET':
-		return JsonResponse({'error': 'Method not allowed'}, status=405)
 	try:
-		logging.info(id)
-		with default_storage.open(f"avatar/{id}", 'rb') as img_file:
-			image = Image.open(img_file).convert("RGB")
-			response = HttpResponse(content_type="image/jpeg")
-			image.save(response, "JPEG")
-			return response
-	except FileNotFoundError:
-		return JsonResponse({'error': 'Image not found.'}, status=404)
-	
+		if request.method != 'GET':
+			return JsonResponse({'error': 'Method not allowed'}, status=405)
+		try:
+			with default_storage.open(f"avatar/{id}", 'rb') as img_file:
+				image = Image.open(img_file).convert("RGB")
+				response = HttpResponse(content_type="image/jpeg")
+				image.save(response, "JPEG")
+				return response
+		except FileNotFoundError:
+			return JsonResponse({'error': 'Image not found.'}, status=404)
+	except:
+		return JsonResponse({'error': 'Bad request'}, status=400)
+
 @login_required
 def api_search_user(request, id):
-	if request.method != 'GET':
-		return JsonResponse({'error': 'Method not allowed'}, status=405)
-	consumer = Token.objects.get(token=request.COOKIES.get('token')).user
-	users = User.objects.filter(nickname__contains=id)
-	response = []
-	for user in users:
-		if user.id != consumer.id:
-			response.append(user.resume_to_json())
-	return JsonResponse(response, safe=False)
+	try:
+		if request.method != 'GET':
+			return JsonResponse({'error': 'Method not allowed'}, status=405)
+		consumer = Token.objects.get(token=request.COOKIES.get('token')).user
+		users = User.objects.filter(nickname__contains=id)
+		response = []
+		for user in users:
+			if user.id != consumer.id:
+				response.append(user.resume_to_json())
+		return JsonResponse(response, safe=False)
+	except:
+		return JsonResponse({'error': 'Bad request'}, status=400)
 
 @login_required
 def api_follow(request, id):
-	if request.method != 'POST':
-		return JsonResponse({'error': 'Method not allowed'}, status=405)
-	user = Token.objects.get(token=request.COOKIES.get('token')).user
-	user_to_follow = get_object_or_404(User, id=id)
-	if user == user_to_follow:
-		return JsonResponse({'error': 'You cannot follow yourself'}, status=400)
-	user.following.add(user_to_follow)
-	following = []
-	for user in user.following.all():
-		following.append(user.resume_to_json())
-	response = {
-		"following": following,
-	}
-	return JsonResponse(response, safe=False)
+	try:
+		if request.method != 'POST':
+			return JsonResponse({'error': 'Method not allowed'}, status=405)
+		user = Token.objects.get(token=request.COOKIES.get('token')).user
+		user_to_follow = get_object_or_404(User, id=id)
+		if user == user_to_follow:
+			return JsonResponse({'error': 'You cannot follow yourself'}, status=400)
+		user.following.add(user_to_follow)
+		following = []
+		for user in user.following.all():
+			following.append(user.resume_to_json())
+		response = {
+			"following": following,
+		}
+		return JsonResponse(response, safe=False)
+	except:
+		return JsonResponse({'error': 'Bad request'}, status=400)
 
 @login_required
 def api_unfollow(request, id):
-	if request.method != 'POST':
-		return JsonResponse({'error': 'Method not allowed'}, status=405)
-	user = Token.objects.get(token=request.COOKIES.get('token')).user
-	user_to_unfollow = get_object_or_404(User, id=id)
-	if user == user_to_unfollow:
-		return JsonResponse({'error': 'You cannot unfollow yourself'}, status=400)
-	user.following.remove(user_to_unfollow)
-	following = []
-	for user in user.following.all():
-		following.append(user.resume_to_json())
-	response = {
-		"following": following,
-	}
-	return JsonResponse(response, safe=False)
+	try:
+		if request.method != 'POST':
+			return JsonResponse({'error': 'Method not allowed'}, status=405)
+		user = Token.objects.get(token=request.COOKIES.get('token')).user
+		user_to_unfollow = get_object_or_404(User, id=id)
+		if user == user_to_unfollow:
+			return JsonResponse({'error': 'You cannot unfollow yourself'}, status=400)
+		user.following.remove(user_to_unfollow)
+		following = []
+		for user in user.following.all():
+			following.append(user.resume_to_json())
+		response = {
+			"following": following,
+		}
+		return JsonResponse(response, safe=False)
+	except:
+		return JsonResponse({'error': 'Bad request'}, status=400)
--- srcs/server/index/views/login.py
+++ srcs/server/index/views/login.py
@@ -22,170 +22,185 @@ def makeid(length):
 
 @login_forbiden
 def login(request):
-	if request.method == "GET":
-		return render(request, 'views/connection.html')
-	elif request.method == "POST":
-		if 'login' not in request.POST or 'password' not in request.POST:
-			return render(request, 'views/connection.html', {"is_invalid": True})
-		username = request.POST['login']
-		password = request.POST['password']
-		if not username or not password:
-			return render(request, 'views/connection.html', {"login": username, "is_invalid": True})
-		
-		if User.objects.filter(username=username).exists():
-			user = User.objects.get(username=username)
-			try :
-				if pbkdf2.verify(password, user.password):
-					token = makeid(100)
-					response = redirect("/")
-					Token.objects.create(token=token, user=user)
-					response.set_cookie(key='token', value=token, httponly=True, expires=7*24*60*60, samesite='Lax')
-					return response
-			except:
+	try:
+		if request.method == "GET":
+			return render(request, 'views/connection.html')
+		elif request.method == "POST":
+			if 'login' not in request.POST or 'password' not in request.POST:
+				return render(request, 'views/connection.html', {"is_invalid": True})
+			username = request.POST['login']
+			password = request.POST['password']
+			if not username or not password:
 				return render(request, 'views/connection.html', {"login": username, "is_invalid": True})
-		return render(request, 'views/connection.html', {"login": username, "is_invalid": True})
-	else :
-		return JsonResponse({'error': 'Method not allowed'}, status=405)
+			
+			if User.objects.filter(username=username).exists():
+				user = User.objects.get(username=username)
+				try :
+					if pbkdf2.verify(password, user.password):
+						token = makeid(100)
+						response = redirect("/")
+						Token.objects.create(token=token, user=user)
+						response.set_cookie(key='token', value=token, httponly=True, expires=7*24*60*60, samesite='Lax')
+						return response
+				except:
+					return render(request, 'views/connection.html', {"login": username, "is_invalid": True})
+			return render(request, 'views/connection.html', {"login": username, "is_invalid": True})
+		else :
+			return JsonResponse({'error': 'Method not allowed'}, status=405)
+	except:
+		return JsonResponse({'error': 'Bad request'}, status=400)
 
 @login_forbiden
 def signup(request):
-	if request.method == "GET":
-		return render(request, 'views/connection.html', { "is_signup": True, "action_url": "/signup"})
-	elif request.method == "POST":
-		if 'login' not in request.POST or 'password' not in request.POST:
-			return render(request, 'views/connection.html', {"is_invalid": True, "is_signup": True, "action_url": "/signup"})
-		username = request.POST['login']
-		password = request.POST['password']
-		if not username or not password:
-			return render(request, 'views/connection.html', {"login": username, "is_invalid": True, "is_signup": True, "action_url": "/signup"})
-		
-		if len(username) < 3 or len(username) > 20 or len(password) < 3 or len(password) > 20:
-			return render(request, 'views/connection.html', {"login": username, "is_signup": True, "action_url": "/signup", "is_invalid": True})
-		
-		if User.objects.filter(username=username).exists():
-			return render(request, 'views/connection.html', {"login": username, "is_signup": True, "action_url": "/signup", "exists": True})
+	try:
+		if request.method == "GET":
+			return render(request, 'views/connection.html', { "is_signup": True, "action_url": "/signup"})
+		elif request.method == "POST":
+			if 'login' not in request.POST or 'password' not in request.POST:
+				return render(request, 'views/connection.html', {"is_invalid": True, "is_signup": True, "action_url": "/signup"})
+			username = request.POST['login']
+			password = request.POST['password']
+			if not username or not password:
+				return render(request, 'views/connection.html', {"login": username, "is_invalid": True, "is_signup": True, "action_url": "/signup"})
+			
+			if len(username) < 3 or len(username) > 20 or len(password) < 3 or len(password) > 20:
+				return render(request, 'views/connection.html', {"login": username, "is_signup": True, "action_url": "/signup", "is_invalid": True})
+			
+			if User.objects.filter(username=username).exists():
+				return render(request, 'views/connection.html', {"login": username, "is_signup": True, "action_url": "/signup", "exists": True})
 
-		password = pbkdf2.hash(password)
-		id = makeid(10)
-		while User.objects.filter(id=id).exists():
+			password = pbkdf2.hash(password)
 			id = makeid(10)
+			while User.objects.filter(id=id).exists():
+				id = makeid(10)
 
-		default_profile_picture = get_new_default_profile_picture()
-		if (default_profile_picture == None):
-			logging.error("Failed to get default profile picture")
-			default_profile_picture = ""
-		user = User.objects.create(id=id, nickname=username, username=username, password=password, default_profile_picture=default_profile_picture)
-		response = redirect("/")
+			default_profile_picture = get_new_default_profile_picture()
+			if (default_profile_picture == None):
+				logging.error("Failed to get default profile picture")
+				default_profile_picture = ""
+			user = User.objects.create(id=id, nickname=username, username=username, password=password, default_profile_picture=default_profile_picture)
+			response = redirect("/")
 
-		token = makeid(100)
-		Token.objects.create(token=token, user=user)
-		response.set_cookie(key='token', value=token, httponly=True, expires=7*24*60*60, samesite='Lax')
-		return response
-	else:
-		return JsonResponse({'error': 'Method not allowed'}, status=405)
+			token = makeid(100)
+			Token.objects.create(token=token, user=user)
+			response.set_cookie(key='token', value=token, httponly=True, expires=7*24*60*60, samesite='Lax')
+			return response
+		else:
+			return JsonResponse({'error': 'Method not allowed'}, status=405)
+	except:
+		return JsonResponse({'error': 'Bad request'}, status=400)
 
 @login_forbiden
 def callback_intra(request):
-	code = request.GET.get('code', '')
-	INTRA_USER = os.environ.get('INTRA_USER')
-	INTRA_SECRET = os.environ.get('INTRA_SECRET')
-	BASE_URI = os.environ.get('BASE_URI')
-
-	data = {
-		'grant_type': 'authorization_code',
-		'client_id': INTRA_USER,
-		'client_secret': INTRA_SECRET,
-		'code': code,
-		'redirect_uri': f'{BASE_URI}/callback/intra'
-	}
 	try:
-		response = requests.post('https://api.intra.42.fr/oauth/token', data=data)
-		response = response.json()
-		access_token = response['access_token']
-		intra_data = requests.get('https://api.intra.42.fr/v2/me', headers={'Authorization': f'Bearer {access_token}'})
-		intra_data = intra_data.json()
-		intra_id = intra_data['login']
+		code = request.GET.get('code', '')
+		INTRA_USER = os.environ.get('INTRA_USER')
+		INTRA_SECRET = os.environ.get('INTRA_SECRET')
+		BASE_URI = os.environ.get('BASE_URI')
 
-		if "image" in intra_data and "versions" in intra_data['image'] and "medium" in intra_data['image']['versions']:
-			default_profile_picture = intra_data['image']['versions']['medium']
-		else:
-			default_profile_picture = get_new_default_profile_picture()
-	except:
-		return redirect("/login")
+		data = {
+			'grant_type': 'authorization_code',
+			'client_id': INTRA_USER,
+			'client_secret': INTRA_SECRET,
+			'code': code,
+			'redirect_uri': f'{BASE_URI}/callback/intra'
+		}
+		try:
+			response = requests.post('https://api.intra.42.fr/oauth/token', data=data)
+			response = response.json()
+			access_token = response['access_token']
+			intra_data = requests.get('https://api.intra.42.fr/v2/me', headers={'Authorization': f'Bearer {access_token}'})
+			intra_data = intra_data.json()
+			intra_id = intra_data['login']
 
-	if not User.objects.filter(intra_id=intra_id).exists():
-		id = makeid(10)
-		while User.objects.filter(id=id).exists():
+			if "image" in intra_data and "versions" in intra_data['image'] and "medium" in intra_data['image']['versions']:
+				default_profile_picture = intra_data['image']['versions']['medium']
+			else:
+				default_profile_picture = get_new_default_profile_picture()
+		except:
+			return redirect("/login")
+
+		if not User.objects.filter(intra_id=intra_id).exists():
 			id = makeid(10)
+			while User.objects.filter(id=id).exists():
+				id = makeid(10)
 
-		user = User.objects.create(id=id, nickname=intra_id, intra_id=intra_id, default_profile_picture=default_profile_picture)
-	else:
-		user = User.objects.get(intra_id=intra_id)
+			user = User.objects.create(id=id, nickname=intra_id, intra_id=intra_id, default_profile_picture=default_profile_picture)
+		else:
+			user = User.objects.get(intra_id=intra_id)
 
-	response = redirect("/")
-	token = makeid(100)
-	while Token.objects.filter(token=token).exists():
+		response = redirect("/")
 		token = makeid(100)
+		while Token.objects.filter(token=token).exists():
+			token = makeid(100)
 
-	Token.objects.create(token=token, user=user)
-	response.set_cookie(key='token', value=token, httponly=True, expires=7*24*60*60, samesite='Lax')
-	return response
+		Token.objects.create(token=token, user=user)
+		response.set_cookie(key='token', value=token, httponly=True, expires=7*24*60*60, samesite='Lax')
+		return response
+	except:
+		return JsonResponse({'error': 'Bad request'}, status=400)
 
 def callback_swivel(request):
-	code = request.GET.get('code', '')
-	SWIVEL_USER = os.environ.get('SWIVEL_USER')
-	SWIVEL_SECRET = os.environ.get('SWIVEL_SECRET')
-	id = None
-
-	data = {
-		'client_id': SWIVEL_USER,
-		'client_secret': SWIVEL_SECRET,
-		'code': code,
-	}
 	try:
-		query_string = urllib.parse.urlencode(data)
-		response = requests.post(f"https://auth0.maxencegama.dev/o/auth/access_token?{query_string}")
-		response = response.json()
-		access_token = response['access_token']
-		swivel_data = requests.get('https://api.maxencegama.dev/user/user.profile', headers={'Authorization': f'Bearer {access_token}'})
-		swivel_data = swivel_data.json()
-		logging.info(swivel_data)
-		swivel_id = swivel_data['id']
-		if swivel_id == "sVHs1WArJ9lk1Y4J9kbk":
-			id = "maxence"
-		swivel_username = swivel_data['username']
-		default_profile_picture = swivel_data['profile_picture']
-	except:
-		return redirect("/login")
+		code = request.GET.get('code', '')
+		SWIVEL_USER = os.environ.get('SWIVEL_USER')
+		SWIVEL_SECRET = os.environ.get('SWIVEL_SECRET')
+		id = None
 
-	if not User.objects.filter(swivel_id=swivel_id).exists():
-		if id is None:
-			id = makeid(10)
-		while User.objects.filter(id=id).exists():
-			id = makeid(10)
+		data = {
+			'client_id': SWIVEL_USER,
+			'client_secret': SWIVEL_SECRET,
+			'code': code,
+		}
+		try:
+			query_string = urllib.parse.urlencode(data)
+			response = requests.post(f"https://auth0.maxencegama.dev/o/auth/access_token?{query_string}")
+			response = response.json()
+			access_token = response['access_token']
+			swivel_data = requests.get('https://api.maxencegama.dev/user/user.profile', headers={'Authorization': f'Bearer {access_token}'})
+			swivel_data = swivel_data.json()
+			logging.info(swivel_data)
+			swivel_id = swivel_data['id']
+			if swivel_id == "sVHs1WArJ9lk1Y4J9kbk":
+				id = "maxence"
+			swivel_username = swivel_data['username']
+			default_profile_picture = swivel_data['profile_picture']
+		except:
+			return redirect("/login")
 
-		user = User.objects.create(id=id, nickname=swivel_username, swivel_id=swivel_id, default_profile_picture=default_profile_picture)
-	else:
-		user = User.objects.get(swivel_id=swivel_id)
+		if not User.objects.filter(swivel_id=swivel_id).exists():
+			if id is None:
+				id = makeid(10)
+			while User.objects.filter(id=id).exists():
+				id = makeid(10)
 
-	response = redirect("/")
-	token = makeid(100)
-	while Token.objects.filter(token=token).exists():
+			user = User.objects.create(id=id, nickname=swivel_username, swivel_id=swivel_id, default_profile_picture=default_profile_picture)
+		else:
+			user = User.objects.get(swivel_id=swivel_id)
+
+		response = redirect("/")
 		token = makeid(100)
+		while Token.objects.filter(token=token).exists():
+			token = makeid(100)
 
-	Token.objects.create(token=token, user=user)
-	response.set_cookie(key='token', value=token, httponly=True, expires=7*24*60*60, samesite='Lax')
-	return response
+		Token.objects.create(token=token, user=user)
+		response.set_cookie(key='token', value=token, httponly=True, expires=7*24*60*60, samesite='Lax')
+		return response
+	except:
+		return JsonResponse({'error': 'Bad request'}, status=400)
 
 @login_required
 def logout(request):
-	cookie = request.COOKIES.get('token')
-	response = redirect("/")
-	if not cookie or not Token.objects.filter(token=cookie).exists():
-		return response
+	try:
+		cookie = request.COOKIES.get('token')
+		response = redirect("/")
+		if not cookie or not Token.objects.filter(token=cookie).exists():
+			return response
 
-	if Token.objects.get(token=cookie).is_valid:
-		token = Token.objects.get(token=cookie)
-		token.is_valid = False
-		token.save()
-	return response
+		if Token.objects.get(token=cookie).is_valid:
+			token = Token.objects.get(token=cookie)
+			token.is_valid = False
+			token.save()
+		return response
+	except:
+		return JsonResponse({'error': 'Bad request'}, status=400)
