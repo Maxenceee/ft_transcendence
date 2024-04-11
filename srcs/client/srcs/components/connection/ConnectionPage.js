83f6cfcb850e96135decc5b0bff87b0c15d33c35
Maxenceee <72473014+Maxenceee@users.noreply.github.com>
2024-04-12 00:43:09 +0200

    ajout de la version one page de la page de connexion

--- srcs/client/srcs/components/connection/ConnectionPage.js
+++ srcs/client/srcs/components/connection/ConnectionPage.js
@@ -1,12 +1,139 @@
-import { Component, createElement } from '..';
+import { Component, createElement, navigate } from '..';
 import axios from 'axios';
 import LoginComp from './LoginComp';
 import RegisterComp from './RegisterComp';
+import SwivelButton from '../auth/SwivelButton';
+
+class InputSection extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { username: "", register: false, invalid: false, exists: false, error: null }
+		this.ref = {current: null};
+		this.formref = {current: null};
+		this.username_ref = {current: null};
+		this.password_ref = {current: null};
+	
+		this.onSubmit = this.onSubmit.bind(this);
+	}
+
+	onSubmit(e) {
+		e.preventDefault();
+		console.log(this.formref.current);
+		let data = new FormData(this.formref.current);
+		console.log(data);
+		this.password_ref.current.value = "";
+		axios.post(this.state.register ? '/signup' : '/login', data)
+		.then(res => res.data)
+		.then(data => {
+			console.log(data);
+			if (data.user && data.authenticated) {
+				return navigate('/');
+			}
+			if (!data.user || !data.authenticated) {
+				this.setState({invalid: true, exists: false});
+			} else if (data.exists) {
+				this.setState({invalid: false, exists: true});
+			}
+		})
+		.catch(error => {
+			console.error(error);
+			this.setState({error: "Une erreur est survenue. Veuillez réessayer plus tard.", invalid: false, exists: false});
+		})
+	}
+
+	render() {
+		return createElement('section', {
+			children: createElement('div', {
+				class: "semi", children: createElement('div', {
+					class: "cntc", children: createElement('div', {
+						class: "auth-form", children: createElement('div', {
+							class: "auth-form-body", children: [
+								this.state.invalid && createElement('div', {
+									children: createElement('div', {
+										class: "auth-unauthorized", children: createElement('div', {
+											class: "in", children: createElement('div', {
+												"aria-atomic": "true", role: "alert", children: [
+													createElement('strong', {
+														children: "Nom d'utilisateur"
+													}),
+													" ou ",
+													createElement('strong', {
+														children: "Mot de passe"
+													}),
+													" incorrect."
+												]
+											})
+										})
+									})
+								}),
+								this.state.exists && createElement('div', {
+									children: createElement('div', {
+										class: "auth-unauthorized", children: createElement('div', {
+											class: "in", children: createElement('div', {
+												"aria-atomic": "true", role: "alert", children: "Ce nom d'utilisateur n'est pas disponible."
+											})
+										})
+									})
+								}),
+								this.state.error && createElement('div', {
+									children: createElement('div', {
+										class: "auth-unauthorized", children: createElement('div', {
+											class: "in", children: createElement('div', {
+												"aria-atomic": "true", role: "alert", children: this.state.error
+											})
+										})
+									})
+								}),
+								createElement('h1', {
+									children: this.state.register ? "Créer un compte" : "Connexion"
+								}),
+								createElement('form', {
+									ref: this.formref, onsubmit: this.onSubmit, "data-turbo": "false", "accept-charset": "UTF-8", children: [
+										this.state.register ?
+										createElement(RegisterComp, {toggle_mode: () => this.setState({register: !this.state.register}), username_ref: this.username_ref, password_ref: this.password_ref})
+										:
+										createElement(LoginComp, {toggle_mode: () => this.setState({register: !this.state.register}), username_ref: this.username_ref, password_ref: this.password_ref}),
+										createElement('div', {
+											class: "card-separator upper-text"
+										}),
+										createElement('div', {
+											class: "external-connect", children: createElement('div', {
+												children: [
+													createElement('div', {
+														id: "intra-button", class: "external-connect-btn intra-button", children: [
+															createElement('img', {
+																class: "icon", src: "/static/images/42_logo_black-684989d43d629b3c0ff6fd7e1157ee04db9bb7a73fba8ec4e01543d650a1c607.png", alt: "icon-42"
+															}),
+															createElement('div', {
+																class: "inner", children: "Sign in with 42 Intra"
+															})
+														]
+													}),
+													createElement('div', {
+														ref: this.ref, class: "external-connect-btn", children: createElement(SwivelButton, {
+															client_id: process.env.SWIVEL_CLIENT_ID,
+															redirect_uri: window.location.origin,
+															callback: window.location.origin.concat("/callback/swivel"),
+															scope: "user:profile",
+															disable_colormode: true
+														})
+													})
+												]
+											})
+										})
+									]
+								})
+							]
+						})
+					})
+				})
+			})
+		})}
+}
 
 class ConnectionPage extends Component {
 	constructor(props) {
 		super(props);
-		this.state = { register: false }
 	}
 
 	render() {
@@ -18,19 +145,11 @@ class ConnectionPage extends Component {
 							src: "/static/videos/0001-0151.mp4", type: "video/mp4"
 						})
 					}),
-					createElement('section', {
-						children: createElement('div', {
-							class: "semi", children: createElement('div', {
-								class: "cntc", children: createElement('div', {
-									class: "auth-form", children: createElement(RegisterComp)
-								})
-							})
-						})
-					})
+					createElement(InputSection)
 				]
 			})
 		})		
 	}
 }
 
-export default ConnectionPage
+export default ConnectionPage;
--- srcs/client/srcs/components/connection/RegisterComp.js
+++ srcs/client/srcs/components/connection/RegisterComp.js
@@ -1,86 +1,38 @@
-import { Component, createElement, link } from '..';
+import { Component, createElement } from '..';
+import Input from './Input';
 
 class RegisterComp extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {login: "", password: ""};
-		this.formref = {current: null};
-		this.onInput = this.onInput.bind(this);
 	}
 
 	render() {
 		return createElement('div', {
-			class: "auth-form-body", children: [
-				createElement('h1', {
-					children: "Créer un compte"
-				}),
-				createElement('form', {
-					ref: this.formref, "data-turbo": "false", "accept-charset": "UTF-8", children: [
-						createElement('div', {
-							class: "auth-form-input", children: [
-								createElement('div', {
-									class: "ttpo form-input", children: createElement('label', {
-										class: "f0n8F"+(this.state.login.length ? " FATdn" : ""), children: [
-											createElement('span', {
-												class: "_9nyy2", children: "Nom d'utilisateur"
-											}),
-											createElement('input', {
-												class: "_2hvTZ pexuQ zyHYP", id: "identifierU", autocomplete: "current-username", minlength: "3", maxlength: "10", autocorrect: "off", autocapitalize: "off", required: "", type: "text", "aria-required": "true", name: "login", value: this.state.login, oninput: this.onInput
-											})
-										]
-									})
-								}),
-								createElement('div', {
-									class: "ttpo form-input", children: createElement('label', {
-										class: "f0n8F"+(this.state.password.length ? " FATdn" : ""), children: [
-											createElement('span', {
-												class: "_9nyy2", children: "Mot de passe"
-											}),
-											createElement('input', {
-												class: "_2hvTZ pexuQ zyHYP", id: "identifierP", autocomplete: "current-password", minlength: "3", maxlength: "10", autocorrect: "off", autocapitalize: "off", required: "", type: "password", "aria-required": "true", "aria-label": "password", name: "password", value: this.state.password, oninput: this.onInput
-											})
-										]
-									})
-								}),
-								createElement('input', {
-									type: "submit", value: "Créer mon compte", class: "auth-form-submit btn-primary", "data-disable-with": "Création…", "data-signin-label": "Créer mon compte", "data-sso-label": "Créer un compte pour accéder à la ressource"
-								})
-							]
+			children: [
+				createElement('div', {
+					class: "auth-form-input", children: [
+						createElement(Input, {
+							ref: this.props.username_ref, label: "Nom d'utilisateur", id: "identifierU", autocomplete: "current-username", name: "login"
 						}),
-						createElement('div', {
-							class: "auth-form-other-l", children: createElement('p', {
-								children: [
-									"Déjà un compte ? ",
-									link({
-										to: "/login", children: "Se connecter"
-									})
-								]
-							})
+						createElement(Input, {
+							ref: this.props.password_ref, label: "Mot de passe", id: "identifierP", autocomplete: "current-password", name: "password"
 						}),
-						createElement('div', {
-							class: "card-separator upper-text"
-						}),
-						createElement('div', {
-							class: "external-connect", children: createElement('div', {
-								children: [
-									createElement('div', {
-										id: "intra-button", class: "external-connect-btn intra-button", children: [
-											createElement('img', {
-												class: "icon", src: "/static/images/42_logo_black-684989d43d629b3c0ff6fd7e1157ee04db9bb7a73fba8ec4e01543d650a1c607.png", alt: "icon-42"
-											}),
-											createElement('div', {
-												class: "inner", children: "Sign in with 42 Intra"
-											})
-										]
-									}),
-									createElement('div', {
-										id: "swivel-ref-h", class: "external-connect-btn"
-									})
-								]
-							})
+						createElement('input', {
+							type: "submit", value: "Créer mon compte", class: "auth-form-submit btn-primary", "data-disable-with": "Création…", "data-signin-label": "Créer mon compte", "data-sso-label": "Créer un compte pour accéder à la ressource"
 						})
 					]
-				})
+				}),
+				createElement('div', {
+					class: "auth-form-other-l", children: createElement('p', {
+						children: [
+							"Déjà un compte ? ",
+							createElement('span', {
+								children: "Se connecter", onclick: this.props.toggle_mode
+							})
+						]
+					})
+				}),
 			]
 		})
 	}
